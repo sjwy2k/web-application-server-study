0d2eadd2545db0b87809b32a0c49b92297225c7e
Jay Shin <sjwy2k@gmail.com>
2022-03-01 16:50:43 +0900

    220301 HttpRequest 클래스 생성, HttpRequestTest 테스트코드 작성

---
+++ src/main/java/http/HttpRequest.java
@@ -0,0 +1,8 @@
+package http;
+
+import java.io.InputStream;
+
+public class HttpRequest {
+
+	
+}
---
+++ src/test/java/http/HttpRequestTest.java
@@ -0,0 +1,42 @@
+package http;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HttpRequestTest {
+
+	private String testDirectory = "./src/test/resources";
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void request_GET() throws Exception {
+		InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+		HttpRequest request = new HttpRequest(in);
+		
+		assertEquals("GET", request.getMethod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("javajigi", request.getParameter("userId"));
+	}
+	
+	@Test
+	public void request_POST() throws Exception {
+		InputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+		HttpRequest request = new HttpRequest(in);
+		
+		assertEquals("POST", request.getMethod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("javajigi", request.getParameter("userId"));
+	}
+
+}
